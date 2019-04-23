4ad7b35be07f42afddf3095fa96ae0196aa8e302
Artem <artem.ustilov@gmail.com>
2019-04-23 16:21:45 +0300

    config init

--- config.php
+++ config.php
@@ -1,1 +1,8 @@
 <?php
+define('PHPGRID_DB_HOSTNAME', 'localhost');
+define('PHPGRID_DB_USERNAME', 'root');
+define('PHPGRID_DB_PASSWORD', '');
+define('PHPGRID_DB_NAME', 'custom_crm');
+define('PHPGRID_DB_TYPE', 'mysql');
+define('PHPGRID_DB_CHARSET','utf8');
+?>
