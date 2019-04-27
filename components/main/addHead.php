79699da1062381a0f0001f2cb7cb9267ec1e37bf
BatyaIsHere <rshchibrik@gmail.com>
2019-04-27 18:07:34 +0300

    head modal added

--- components/main/addHead.php
+++ components/main/addHead.php
@@ -2,18 +2,26 @@
 if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['branch'])) {
     include_once("../../bd/index.php");
     include_once("../../funcs.php");
-    $name = clean($_POST['name']);
+    $first_name = clean($_POST['first_name']);
+    $last_name = clean($_POST['last_name']);
+    $branch = clean($_POST['branch']);
     session_start();
     $user_id = $_SESSION['id'];
     $user_data = mysqli_fetch_assoc($mysql_connect->query("SELECT * FROM users WHERE user_id='$user_id'"));
-    if ($user_data && ($user_data['role'] == "admin" || $user_data['role'] == "sub-admin" || $user_data['role'] == "manager")) {
-        $check = mysqli_fetch_assoc($mysql_connect->query("SELECT * FROM branch WHERE branch_name='$name'"));
-        if ($check) {
-            echo "exists";
+    if ($user_data && ($user_data['role'] == "admin" || $user_data['role'] == "sub-admin" || $user_data['role'] == "manager"
+            || $user_data['role'] == "moder")) {
+//        $check = mysqli_fetch_assoc($mysql_connect->query("SELECT * FROM branch WHERE branch_name='$name'"));
+//        if ($check) {
+//            echo "exists";
+//            return false;
+//        }
+        $branch_data = mysqli_fetch_assoc($mysql_connect->query("SELECT * FROM branch WHERE branch_id='$branch'"));
+        if (!$branch_data) {
+            echo "doesNotExists";
             return false;
         }
         $res = $mysql_connect->
-        query("INSERT INTO branch (branch_name) VALUES(\"$name\")");
+        query("INSERT INTO `owners` (`branch_id`, `last_name`,  `first_name`) VALUES($branch,'$last_name', '$first_name')");
         if ($res) {
             echo "success";
             return false;
