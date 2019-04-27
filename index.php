50f968616110c676d554f75c5d4d2e111257c604
Artem <artem.ustilov@gmail.com>
2019-04-27 14:09:45 +0300

    tabl eon main page + height of tables

--- index.php
+++ index.php
@@ -33,12 +33,11 @@ $sumDebts = $sumDebtsRaw ? mysqli_fetch_assoc($sumDebtsRaw) : null;
 if($sumDebts) $table .= '<h2>Всего: '.($sumDebts["sum"] ? $sumDebts["sum"] : 0).' грн</h2>';
 
 $table .= display_data($connection -> query('
-SELECT concat(C.last_name, " ", C.first_name) AS "Полное имя", byname AS Имя, phone_number AS телефон, email AS почта, R.rollback_sum AS откат
+SELECT concat(C.last_name, " ", C.first_name) AS "Полное имя", byname AS Имя, phone_number AS телефон, email AS почта, C.rollback_sum AS откат
 FROM clients C
-INNER JOIN rollback_paying R ON R.client_id = C.client_id
-WHERE R.rollback_sum > 0 AND R.user_id = '.$_SESSION["id"].'
-ORDER BY R.rollback_sum DESC
-'), "","Откаты");
+INNER JOIN orders O ON O.client_id = C.client_id
+WHERE C.rollback_sum > 0 AND O.user_id = '.$_SESSION["id"].'
+'), "","Ожидают откаты");
 
 $sumDebtsRaw = $connection -> query('
 SELECT SUM(R.rollback_sum) AS sum
