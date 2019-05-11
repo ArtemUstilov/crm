e81b942f8c0677f71ba66d2c443bd3e851b76519
unknown <rshchibrik@gmail.com>
2019-05-11 16:36:16 +0300

    merging

--- components/modals/payRollback.php
+++ components/modals/payRollback.php
@@ -20,9 +20,19 @@ function rollbackModal($data)
 <select id="clientField" data-validation="required">
   <option value="" selected disabled>Выберите клиента</option>';
     foreach ($copy_of_data as $key => $var) {
-        $output .= '<option sum="'.$var['rollback_sum'].'" value="' . $var['login'] . '">' . $var['client_name'] . ' (' . $var['login'] . ')</option>';
+        $output .= '<option sum="'.$var['rollback_sum'].'" value="' . $var['login'] . '"  fiat="' . $var['fiat'] . '">' . $var['client_name'] . ' (' . $var['login'] . ')</option>';
     }
     $output .= '
+</select>
+</p>
+
+<p>
+<select id="FiatField" data-validation="required"><option value="" disabled selected>Выберите валюту</option>';
+        foreach ($more_data['fiat'] as $key => $var) {
+            $output .= '<option value="' . $var["fiat_id"] . '">' . $var['full_name'] . '</option>';
+        }
+
+            $output .= '
 </select>
 </p>
   <p>
--- referals.php
+++ referals.php
@@ -9,23 +9,23 @@ switch (accessLevel()) {
     case 3:
         $info = $connection -> query("
 SELECT B.branch_name AS отдел, concat(U.last_name, ' ', U.first_name) AS агент,  U.login AS 'логин агента', concat(C.last_name, ' ', C.first_name) AS клиент, 
-O.rollback_sum AS сумма,
-O.date AS дата
+concat(O.rollback_sum, ' ', F.name) AS сумма, O.date AS дата
 FROM rollback_paying O
 INNER JOIN clients C ON C.client_id = O.client_id 
 INNER JOIN users U ON U.user_id = O.user_id
 INNER JOIN branch B ON B.branch_id = U.branch_id
+INNER JOIN fiats F ON O.fiat_id = F.fiat_id
 ORDER BY `date` DESC
 ");
         break;
     case 2:
         $info = $connection -> query("
 SELECT concat(U.last_name, ' ', U.first_name) AS агент,  U.login AS 'логин агента', concat(C.last_name, ' ', C.first_name) AS клиент, 
-O.rollback_sum AS сумма,
-O.date AS дата
+concat(O.rollback_sum, ' ', F.name) AS сумма, O.date AS дата
 FROM rollback_paying O
 INNER JOIN clients C ON C.client_id = O.client_id 
 INNER JOIN users U ON U.user_id = O.user_id
+INNER JOIN fiats F ON O.fiat_id = F.fiat_id
 WHERE U.branch_id = '$branch_id'
 ORDER BY `date` DESC
 ");
@@ -33,11 +33,11 @@ ORDER BY `date` DESC
     case 1:
         $info = $connection -> query('
 SELECT concat(C.last_name, " ", C.first_name) AS клиент, 
-O.rollback_sum AS сумма,
-O.date AS дата
+concat(O.rollback_sum, " ", F.name) AS сумма, O.date AS дата
 FROM rollback_paying O
 INNER JOIN clients C ON C.client_id = O.client_id 
 INNER JOIN users U ON U.user_id = O.user_id
+INNER JOIN fiats F ON O.fiat_id = F.fiat_id
 WHERE O.user_id = '.$_SESSION["id"].'
 ORDER BY `date` DESC
 ');
@@ -52,9 +52,9 @@ $options['btn'] = 1;
 $options['btn-text'] = 'Выплатить';
 echo template(display_data($info, $options, $connection -> query('
 SELECT concat(last_name, " ", first_name) AS client_name, 
-byname AS login, rollback_sum
-FROM clients
-WHERE  rollback_sum > 0
+byname AS login, P.sum
+FROM clients C INNER JOIN payments P ON C.client_id = P.client_rollback_id
+WHERE  P.sum > 0
 ')));
 ?>
 
