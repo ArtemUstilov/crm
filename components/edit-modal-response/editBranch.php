ca2bacbf80997ddda62d07771101534c242133b3
Artem <artem.ustilov@gmail.com>
2019-06-02 16:53:04 +0300

    fix branch renaming

--- components/edit-modal-response/editBranch.php
+++ components/edit-modal-response/editBranch.php
@@ -1,16 +1,15 @@
 <?php
-if (isset($_POST['name']) && isset($_POST['money'])) {
+if (isset($_POST['name'])) {
     include_once("../../db.php");
     include_once("../../funcs.php");
     $name = clean($_POST['name']);
-    $money = clean($_POST['money']);
     $branch_id = clean($_POST['branch_id']);
     session_start();
     $user_id = $_SESSION['id'];
     $user_data = mysqli_fetch_assoc($connection->query("SELECT * FROM users WHERE user_id='$user_id'"));
     if ($user_data && (heCan($user_data['role'], 2))) {
         $res = $connection->
-        query("UPDATE branch SET branch_name='$name', money='$money' WHERE branch_id='$branch_id'");
+        query("UPDATE branch SET branch_name='$name' WHERE branch_id='$branch_id'");
         if ($res) {
             echo "edit-success";
             return false;
--- components/selectors/Branch.php
+++ components/selectors/Branch.php
@@ -4,11 +4,11 @@ include_once("../../funcs.php");
 session_start();
 $branch_id = isset($_POST['branch_id']) ? clean($_POST['branch_id']) : $_SESSION['branch_id'];
 $branch_data = $connection->query("
-            SELECT branch_name AS 'name', branch_id AS `id`, money
+            SELECT branch_name AS 'name', branch_id AS `id`
             FROM branch 
             WHERE branch_id = '$branch_id'
             ");
-$branch_data = !$branch_data ?: mysqli_fetch_assoc($branch_data);
+$branch_data = $branch_data ? mysqli_fetch_assoc($branch_data) : null;
 
 if ($branch_data) {
     echo json_encode($branch_data);
